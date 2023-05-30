406b9f75b57e633e8325f9e5a52377e8a30b76b0
aakan <aakan@Aakanksha2410>
2023-05-30 11:30:03 +0530

    [akki]:practice demo class

---
+++ src/Demo/d1.java
@@ -0,0 +1,7 @@
+package Demo;
+
+public class d1 {
+public static void main(String[] args) {
+	System.out.println("hiiiii");
+}
+}
---
+++ src/Demo/d2.java
@@ -0,0 +1,8 @@
+package Demo;
+
+public class d2 {
+	public static void main(String[] args) {
+		System.out.println("heloooooo");
+	}
+
+}
